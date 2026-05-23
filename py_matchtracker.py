5051c87586a7c4521f9c936fdd486f2b5a01d1a3
Eric Reitmaier <eric.reitmaier@gmail.com>
2026-05-23 12:52:57 +0200

    Change comment

--- py_matchtracker.py
+++ py_matchtracker.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# ZaVr2 Match App - Pythonista (English)
+# Football Match Tagger  App - Pythonista (English)
 # Place match.yaml in the same folder as this script
 
 import collections
